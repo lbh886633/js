888b3c615756179971e37a0595b3a49dc19ee30d
cc <cc@cc.cc>
2020-12-27 19:06:59 +0800

    2

--- mi6.js
+++ mi6.js
@@ -1,10 +1,10 @@
 // 本次储存检测
 let rootPath = files.path("./")
 log("本次文件路径", rootPath)
-files.listDir(rootPath, element => {
+files.listDir(rootPath, (element) => {
     log(element)
     let d = files.join(rootPath, element)
     if (files.isDir(d)) {
-        files.listDir(d, e => { console.info(d + " " + e) })
+        files.listDir(d, (e) => { console.info(d + " " + e) })
     }
 });
