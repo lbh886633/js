e48e808be72c4b5cd87e8072835f09387e8cd28c
cc <cc@cc.cc>
2020-12-27 23:54:22 +0800

    1

--- mi6.js
+++ mi6.js
@@ -14,7 +14,7 @@ log("路径",rp)
 log(
     files.move("/sdcard/xxsq/头像列表/1.png", rp)
 )
-lof(files.isFile("/sdcard/xxsq/头像列表/1.png"))
+log(files.isFile("/sdcard/xxsq/头像列表/1.png"))
 exit();
 let rp视频列表 = files.join(rp, "视频列表");
 let rp头像列表 = files.join(rp, "头像列表");
