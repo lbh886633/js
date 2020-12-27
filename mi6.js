4822747c8f7c4086ec25684015a602cc04b67397
cc <cc@cc.cc>
2020-12-27 23:11:28 +0800

    1

--- mi6.js
+++ mi6.js
@@ -1,7 +1,7 @@
 
-let rp = files.path("./")
+let rp = files.path("./")+"teset.txt"
 log(rp)
 // log(files.listDir(rp,function(e){return true;}))
-files.witre(files.join(rp,"teset.txt"), "这是用于测试的一段文字")
+files.witre(rp, "这是用于测试的一段文字")
  
   
