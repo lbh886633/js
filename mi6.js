add6cb831e0a4650628cf825d7794f44dba8eee9
cc <cc@cc.cc>
2020-12-27 23:43:34 +0800

    移动测试

--- mi6.js
+++ mi6.js
@@ -8,3 +8,16 @@ log(
 files.write(rp, "这是用于测试的一段文字")
  
   
+    
+let rp = files.path("./");
+rp = files.join(rp,"xxsq");
+let rp视频列表 = files.join(rp, "视频列表");
+let rp头像列表 = files.join(rp, "头像列表");
+let 视频列表 = "/sdcard/xxsq/视频列表"
+let 头像列表 = "/sdcard/xxsq/头像列表"
+// 确保存在路径
+files.ensureDir(files.join(rp, "1"));
+log("移动结束",
+    files.move(视频列表, rp视频列表),
+    files.move(头像列表, rp头像列表)
+);
