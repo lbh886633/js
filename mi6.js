89faa630a95a296db4392104112241b42fadd369
cc <cc@cc.cc>
2020-12-28 00:03:49 +0800

    4

--- mi6.js
+++ mi6.js
@@ -9,6 +9,20 @@
  
 let rp = files.path("./");
 log(rp)
+
+rp = files.join(rp,"1.png");
+
+log("路径",rp)
+
+log(
+    files.move("/sdcard/xxsq/头像列表/1.png", rp)
+)
+
+log(files.isFile(rp))
+
+exit();
+let rp = files.path("./");
+log(rp)
 rp = files.join(rp,"xxsq");
 log("路径",rp)
 let img = files.readBytes("/sdcard/xxsq/头像列表/1.png")
@@ -18,17 +32,3 @@ log(
 )
 
 log(files.isFile("/sdcard/xxsq/头像列表/1.png"))
-exit();
-let rp视频列表 = files.join(rp, "视频列表");
-let rp头像列表 = files.join(rp, "头像列表");
-let 视频列表 = "/sdcard/xxsq/视频列表"
-let 头像列表 = "/sdcard/xxsq/头像列表"
-// 确保存在路径
-files.ensureDir(files.join(rp, "1"));
-log("移动结束",
-    files.move(视频列表, rp视频列表),
-    files.move(头像列表, rp头像列表)
-);
-log(
-    files.listDir(rp)
-)
