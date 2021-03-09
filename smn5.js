8d24c651ed665b00fa4afea4271a3d92a672e58b
L <L@qq.com>
2021-03-09 23:19:33 +0800

    5

--- smn5.js
+++ smn5.js
@@ -9,7 +9,7 @@ var fasle = false;
         "优化",
         "修复",
         "优化账号注册",
-        "测试_查看参数",
+        "测试_v5",
     ];
     uti = logs.pop();
 }
@@ -796,8 +796,11 @@ function 主程序() {
         }
 
         try{
+            log("获取滑块范围")
             var 滑块范围 = indexInParent(1).depth(8).classNameEndsWith("view.View").findOne(2000)
+            log("滑块范围", 滑块范围)
             if (滑块范围) {
+                log("计算坐标")
                 var 坐标 = 滑块范围.bounds()
                 var clip = images.clip(captureScreen(), 坐标.left, 坐标.top, 坐标.right - 坐标.left, 坐标.bottom - 坐标.top);
                 log("截图打码")
@@ -826,6 +829,7 @@ function 主程序() {
         console.info("测试结束");
         return false;
     }
+    console.error("-----------------")
 
     if(ui.createAccount.checked){
         log("邮箱生成");
